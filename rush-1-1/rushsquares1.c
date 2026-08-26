1b854e78b3220faed8590764f26609d6d76402aa
johan <johan@epitech.eu>
2026-08-26 09:54:44 +0400

    voici le 1er

---
+++ rush-1-1/rushsquares1.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+void my_putchar(char c);
+
+void rush(int x, int y)
+{
+    if (x <= 0 || y <= 0) {
+        write(2, "Invalid size\n", 13);
+        return;
+    }
+    for (int row = 0; row < y; row++){
+        for (int col = 0; col < x; col++){
+            int is_top    = (row == 0);
+            int is_bottom = (row == y - 1);
+            int is_left   = (col == 0);
+            int is_right  = (col == x - 1);
+            int is_horizontal_edge = is_top || is_bottom;
+            int is_vertical_edge   = is_left || is_right;
+            
+            if (is_horizontal_edge && is_vertical_edge) {
+                my_putchar('o');}
+            else if (is_horizontal_edge) {
+                my_putchar('-');}
+            else if (is_vertical_edge) {
+                my_putchar('|');}
+            else {
+                my_putchar(' ');}
+        }my_putchar('\n');
+    }
+} 
